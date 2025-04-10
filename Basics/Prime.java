115b5af66b0f76605fe187fc90517624cd4c30eb
Prince <prince.mehta2026@gmail.com>
2025-04-10 06:39:30 +0530

    optimised checkprime code added

--- Basics/Prime.java
+++ Basics/Prime.java
@@ -1,6 +1,7 @@
 //Check whether number is prime or not
 public class Prime {
 
+    //Method 1
     //Only for n>=2
     public static boolean checkPrime(int num){
         //Corner Case
@@ -15,9 +16,19 @@ public class Prime {
         }
         return true;
     }
+
+    //Method 2 - Optimised
+    public static boolean isPrime(int num){
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num%i == 0){ //completely divided - Composite
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         System.out.println(checkPrime(5));
         System.out.println(checkPrime(4));
-        System.out.println(checkPrime(8));       
+        System.out.println(isPrime(13));       
     }
 }
