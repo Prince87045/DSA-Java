aa0261679f6b62207bf3c450d844714dfd45b7e1
Prince <prince.mehta2026@gmail.com>
2025-04-13 23:12:56 +0530

    Print all pair Array code added

---
+++ Arrays/PairArray.java
@@ -0,0 +1,14 @@
+public class PairArray {
+    public static void printAllArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print("(" + arr[i] + ',' + arr[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,2,1,6,9};
+        printAllArray(arr);
+    }
+}
