7ac0c3b22544830799e8a6e620877fef22bfa38b
Prince <prince.mehta2026@gmail.com>
2025-05-12 17:43:57 +0530

    Sell Stocks

---
+++ Arrays/SellStocks.java
@@ -0,0 +1,20 @@
+//Return the maximum profit you can achieve from the transaction
+public class SellStocks {
+    public static int profit(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<prices.length; i++){
+            if(prices[i] > buyPrice){
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(profit,maxProfit);
+            }else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(profit(prices));
+    }
+}
